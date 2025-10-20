2b1080df6bef4ac3723fb35d5c7156015410499a
Marco Corradini <marco.corradini@itsincom.it>
2025-10-20 14:44:00 +0200

    - Fixato footer - Aggiunta visualizzazione video dal canale formula 1 youtube per channel ID

--- Angular/Open F1/OpenF1/src/app/app.ts
+++ Angular/Open F1/OpenF1/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from './menu/menu';
 import { Footer } from "./footer/footer";
+import { Home } from "./home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu, Footer],
+  imports: [RouterOutlet, Menu, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Angular/Open F1/OpenF1/src/app/home/home.ts
+++ Angular/Open F1/OpenF1/src/app/home/home.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css'],
+  imports: [NgIf],
+  standalone: true,
 })
-export class Home {
+export class Home implements OnInit {
+  videoUrl: SafeResourceUrl | null = null;
+  private readonly CHANNEL_ID = 'UCd8iY-kEHtaB8qt8MH--zGw';
 
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    const uploadsPlaylist = this.CHANNEL_ID.replace(/^UC/, 'UU');
+    const embed = `https://www.youtube.com/embed?listType=playlist&list=${uploadsPlaylist}`;
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(embed);
+  }
 }
